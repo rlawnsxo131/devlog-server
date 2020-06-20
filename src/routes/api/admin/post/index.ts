548aa5b5faaa32d3582be97fd5a1132c2914fc16
juntae <rlawnsxo131@gmail.com>
2020-06-21 01:32:58 +0900

    declaration post end point

--- src/routes/api/admin/post/index.ts
+++ src/routes/api/admin/post/index.ts
@@ -1,8 +1,18 @@
 import * as Router from '@koa/router';
-import { getPost } from './post.ctrl';
+import {
+  getPosts,
+  getPost,
+  createPost,
+  updatePost,
+  deletePost,
+} from './post.ctrl';
 
 const post = new Router();
 
+post.get('/', getPosts);
 post.get('/:id', getPost);
+post.post('/', createPost);
+post.patch('/:id', updatePost);
+post.delete('/:id', deletePost);
 
 export default post;
--- src/routes/api/admin/post/post.ctrl.ts
+++ src/routes/api/admin/post/post.ctrl.ts
@@ -4,3 +4,19 @@ export const getPost: Middleware = ctx => {
   const { id } = ctx.params;
   ctx.body = `${id} - getPost`;
 };
+
+export const getPosts: Middleware = ctx => {
+  ctx.body = 'getPosts';
+};
+
+export const createPost: Middleware = ctx => {
+  ctx.body = 'createPosts';
+};
+
+export const updatePost: Middleware = ctx => {
+  ctx.body = 'updatePosts';
+};
+
+export const deletePost: Middleware = ctx => {
+  ctx.body = 'deletePosts';
+};
