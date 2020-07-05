b03b3b97a82696824de3c669bbe1d87d53ec7460
juntae <rlawnsxo131@gmail.com>
2020-07-06 01:22:28 +0900

    little bit fixed

--- src/routes/api/admin/auth/auth.ctrl.ts
+++ src/routes/api/admin/auth/auth.ctrl.ts
@@ -5,13 +5,24 @@ import { normalizedString, decrypt } from '../../../../lib/utils';
 import { generateToken } from '../../../../lib/token';
 
 export const authCheck: Middleware = async ctx => {
-  if (!ctx.state.user_id) throw new Error('Not Found user_id');
-  const adminRepo = getRepository(AdminUser);
-  const adminUser = await adminRepo.findOne(ctx.user_id);
-  if (!adminUser) throw new Error('Not Found admin_user');
-  ctx.body = {
-    email: adminUser.email,
-  };
+  if (!ctx.state.user_id) {
+    ctx.status = 400;
+    return;
+  }
+
+  try {
+    const adminRepo = getRepository(AdminUser);
+    const adminUser = await adminRepo.findOne(ctx.state.user_id);
+    if (!adminUser) {
+      ctx.status = 400;
+      return;
+    }
+    ctx.body = {
+      email: adminUser.email,
+    };
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 type SignInParams = {
@@ -20,26 +31,23 @@ type SignInParams = {
 };
 export const signIn: Middleware = async ctx => {
   const { email, password }: SignInParams = ctx.request.body;
-
   if (!normalizedString(email) || !normalizedString(password)) {
     ctx.status = 400;
-    ctx.body = { msg: 'Check Email or Password' };
-    return;
-  }
-
-  const adminUserRepo = getRepository(AdminUser);
-  const adminUser = await adminUserRepo.findOne({ email });
-  const decryptPassword = adminUser
-    ? await decrypt(password, adminUser.salt)
-    : undefined;
-
-  if (!adminUser || decryptPassword !== adminUser.password) {
-    ctx.status = 400;
-    ctx.body = 'Wrong Email or Password';
     return;
   }
 
   try {
+    const adminUserRepo = getRepository(AdminUser);
+    const adminUser = await adminUserRepo.findOne({ email });
+    const decryptPassword = adminUser
+      ? await decrypt(password, adminUser.salt)
+      : undefined;
+
+    if (!adminUser || decryptPassword !== adminUser.password) {
+      ctx.status = 400;
+      return;
+    }
+
     const token = await generateToken({ user_id: adminUser.id, email });
     ctx.cookies.set('access_token', token, {
       httpOnly: true,
@@ -51,13 +59,14 @@ export const signIn: Middleware = async ctx => {
     };
   } catch (e) {
     console.error(e);
-    ctx.throw(e, 500);
+    ctx.throw(500, e);
   }
 };
 
 export const signOut: Middleware = async ctx => {
   if (!ctx.state.user_id) {
-    ctx.throw(400, 'Not Logged In');
+    ctx.status = 400;
+    return;
   }
   ctx.cookies.set('access_token', '', { maxAge: 0, httpOnly: true });
   ctx.status = 204;
