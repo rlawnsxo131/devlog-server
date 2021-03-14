d6acaa74232fe4084b76fbc2e123441c4bf90a3f
juntae <rlawnsxo131@gmail.com>
2021-03-14 21:46:38 +0900

    add valid function to checkUrl middleware

--- src/lib/middlewares/checkUrl.ts
+++ src/lib/middlewares/checkUrl.ts
@@ -1,15 +1,31 @@
 import { Middleware } from 'koa';
 
 const checkUrl: Middleware = (ctx, next) => {
-  if (/^\/graphql/.test(ctx.path)) {
-    return next();
+  const allowedHosts = [
+    /^https:\/\/devlog.juntae.kim$/,
+    /^https:\/\/john-admin.juntae.kim$/,
+  ];
+
+  if (process.env.NODE_ENV === 'development') {
+    allowedHosts.push(/^http:\/\/localhost/);
   }
-  const allowUrl = [/^\/health/, /^\/rss/, /^\/atom/, /^\/sitemaps/];
-  const valid = allowUrl.some((regex) => regex.test(ctx.path));
+
+  const { origin } = ctx.headers;
+  const valid = allowedHosts.some((regex) => regex.test(origin));
+
   if (!valid) {
-    ctx.status = 400;
-    return;
+    if (/^\/graphql/.test(ctx.path)) {
+      ctx.staus = 400;
+      return;
+    }
+    const allowUrl = [/^\/health/, /^\/rss/, /^\/atom/, /^\/sitemaps/];
+    const validUrl = allowUrl.some((regex) => regex.test(ctx.path));
+    if (!validUrl) {
+      ctx.status = 400;
+      return;
+    }
   }
+
   return next();
 };
 
