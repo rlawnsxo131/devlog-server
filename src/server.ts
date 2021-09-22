20d40ce1c3e09068778d0b26c9b76ecf8ae8db6e
juntae <rlawnsxo131@gmail.com>
2021-09-23 04:02:23 +0900

    remove async keyword

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import Database from './database';
 
 const { PORT } = process.env;
 const database = new Database();
-database.getConnection().then(async () => {
+database.getConnection().then(() => {
   app.listen(PORT, () => {
     console.log(`Server listening to PORT ${PORT}`);
   });
