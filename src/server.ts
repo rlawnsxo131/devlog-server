cd0cc8e5c695c3c210b6143b593e8154c4deb82b
juntae <rlawnsxo131@gmail.com>
2021-09-23 22:36:38 +0900

    modify of local server start codes

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,10 @@ import Database from './database';
 
 const { PORT } = process.env;
 const database = new Database();
-database.getConnection().then(async () => {
-  await apollo.start();
+(async () => {
+  await Promise.all([database.getConnection(), apollo.start()]);
   apollo.applyMiddleware({ app, cors: false });
   app.listen(PORT, () => {
     console.log(`Server listening to PORT ${PORT}`);
   });
-});
+})();
